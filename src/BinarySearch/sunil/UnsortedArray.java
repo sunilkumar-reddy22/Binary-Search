73d9acd830600b105970e3162b5831c9ab690b21
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-07 21:24:55 +0530

    Added unsorted binary search program

---
+++ src/BinarySearch/sunil/UnsortedArray.java
@@ -0,0 +1,31 @@
+package BinarySearch.sunil;
+
+public class UnsortedArray {
+    public static void main(String[] args) {
+        //unsorted array.
+        int[] nums = {90, 70, 18, 12, 6, 4, 3, 1};
+        int target = 6;
+        int result = Find(nums, target);
+        System.out.println(result);
+    }
+
+    static int Find(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length -1;
+        int ans = -1;
+        //running while loop indicating how many val are left.
+        while (start <= end) {
+            int mid = start + (end - start)/2;
+            if (nums[mid] == target) {
+                return mid;
+
+            } else if (nums[mid] < target) {
+                end = mid - 1;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
+
