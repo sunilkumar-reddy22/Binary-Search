46c6e1d6f6f69cd9696bf3fc242d9298a8d98a41
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-05-29 21:01:56 +0530

    Added mountain array -->approching in binary searchng method

---
+++ src/Leetcode/Mountainarray.java
@@ -0,0 +1,24 @@
+package Leetcode;
+
+public class Mountainarray {
+    public static void main(String[] args) {
+        int[] nums = {0, 10,0};
+       int ans =  search(nums);
+        System.out.println(ans);
+    }
+
+    static int search(int[] nums) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start < end) {
+            int mid = start + (end - start) / 2;
+            //if mid is greater than the mid + 1 the it is in the decending order. so we store the mid val in the end.
+            if (nums[mid] > nums[mid + 1]) {
+                end = mid;
+            } else {
+                start = mid + 1;
+            }
+        }
+        return start; //or the end,both start and end points the largest element in the array
+    }
+}
