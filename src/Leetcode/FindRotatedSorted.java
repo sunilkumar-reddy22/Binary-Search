e9e187c50aeda7c8b10be648bc5439db31e6dff3
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-06-14 20:50:48 +0530

    Added leetcode array is sorted and rotated

---
+++ src/Leetcode/FindRotatedSorted.java
@@ -0,0 +1,22 @@
+package Leetcode;
+
+public class FindRotatedSorted {
+    public static void main(String[] args) {
+        int [] nums = {3,4,5,1,2};
+      boolean ans = find(nums);
+        System.out.println(ans);
+    }
+    static boolean find(int [] nums){
+        int count = 0;
+        int n = nums.length;
+        for(int i = 0 ; i < n; i++){
+            if(nums[i] > nums[(i+1) % n]){
+                count++;
+            }
+            if(count > 1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
