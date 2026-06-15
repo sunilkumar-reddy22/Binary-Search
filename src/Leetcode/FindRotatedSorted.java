7f74eff8b05726120c58648b494a5bf3c695d4e3
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-06-15 21:16:51 +0530

    Added comment

--- src/Leetcode/FindRotatedSorted.java
+++ src/Leetcode/FindRotatedSorted.java
@@ -12,7 +12,7 @@ public class FindRotatedSorted {
         for(int i = 0 ; i < n; i++){
             if(nums[i] > nums[(i+1) % n]){
                 count++;
-            }
+            }//count should not be more than one in this case.
             if(count > 1){
                 return false;
             }
