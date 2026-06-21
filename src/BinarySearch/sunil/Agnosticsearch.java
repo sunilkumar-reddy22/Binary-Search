6111fe7362d9011dd07e1bdb720317a12ffb25ae
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-06-21 18:26:53 +0530

    Added agnostic Binary search

---
+++ src/BinarySearch/sunil/Agnosticsearch.java
@@ -0,0 +1,26 @@
+package BinarySearch.sunil;
+
+public class Agnosticsearch {
+    public static void main(String[] args) {
+        int[] nums = {-1, 0, 3, 5, 9, 12};
+        int target = 9;
+     int ans =  search(nums, target);
+        System.out.println(ans);
+    }
+
+    static int search(int[] nums, int target) {
+        int start = 0;
+        int end = nums.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return -1;
+    }
+}
