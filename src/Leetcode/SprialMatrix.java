dffcb0ba455aae12d2e91ec2a541a4d9d10a48d7
sunil kumar reddy <sunilkumarreddy6109@gmail.com>
2026-07-01 22:22:47 +0530

    sprial matrix practice

---
+++ src/Leetcode/SprialMatrix.java
@@ -0,0 +1,13 @@
+package Leetcode;
+
+public class SprialMatrix {
+    public static void main(String[] args) {
+        int [][] nums = {
+                {1,2,3},
+                {3,5,6},
+                {7,8,9},
+
+        };
+        static int []
+    }
+}
